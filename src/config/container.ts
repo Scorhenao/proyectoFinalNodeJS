7b26120f50a119510ab101e0423aea913fbe7043
scorhenao <samcorhenao@gmail.com>
2024-08-02 20:32:48 -0500

    coment the functionality of registerSingleton method

--- src/config/container.ts
+++ src/config/container.ts
@@ -4,7 +4,7 @@ import UserRepository from '../repositories/userRepository';
 import TaskRepository from '../repositories/taskRepository';
 import TaskService from '../services/taskService';
 
-container.registerSingleton<UserRepository>(UserRepository);
+container.registerSingleton<UserRepository>(UserRepository); //Instancia global 
 container.registerSingleton<UserService>(UserService);
 container.registerSingleton<TaskRepository>(TaskRepository);
 container.registerSingleton<TaskService>(TaskService);
