d48098b7427109d29992143115c0c49fce0a8da1
scorhenao <samcorhenao@gmail.com>
2024-08-04 01:19:17 -0500

    comments explaining user.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,46 +1,51 @@
 import {
-    Table,
-    Column,
-    Model,
-    DataType,
-    PrimaryKey,
-    AutoIncrement,
-    HasMany,
-  } from "sequelize-typescript";
-  import { Task } from './task';
-  
-  @Table({
-    tableName: "users",
-    timestamps: true, // Si deseas que Sequelize maneje los timestamps automáticamente
-  })
-  export class User extends Model {
-    @PrimaryKey
-    @AutoIncrement
-    @Column({
+  Table, // Decorador para definir una tabla de la base de datos.
+  Column, // Decorador para definir una columna de la tabla.
+  Model, // Clase base para todos los modelos.
+  DataType, // Tipos de datos de Sequelize.
+  PrimaryKey, // Decorador para definir una clave primaria.
+  AutoIncrement, // Decorador para definir una columna con incremento automático.
+  HasMany, // Decorador para definir una relación de uno a muchos.
+} from "sequelize-typescript";
+import { Task } from './task'; // Importa el modelo Task para establecer relaciones.
+
+@Table({
+  tableName: "users", // Nombre de la tabla en la base de datos.
+  timestamps: true, // Habilita la gestión automática de timestamps (createdAt y updatedAt).
+})
+export class User extends Model {
+  // Define la columna 'id' como clave primaria con incremento automático.
+  @PrimaryKey
+  @AutoIncrement
+  @Column({
       type: DataType.INTEGER,
-    })
-    id!: number;
-  
-    @Column({
+  })
+  id!: number;
+
+  // Define la columna 'name' con tipo STRING que no puede ser nula.
+  @Column({
       type: DataType.STRING,
       allowNull: false,
-    })
-    name!: string;
-  
-    @Column({
-      type: DataType.STRING,
-      allowNull: false,
-      unique: true,
-    })
-    email!: string;
-  
-    @Column({
+  })
+  name!: string;
+
+  // Define la columna 'email' con tipo STRING que no puede ser nula y debe ser única.
+  @Column({
       type: DataType.STRING,
       allowNull: false,
       unique: true,
-    })
-    password!: string;
-  
-    @HasMany(() => Task)
-    task!: Task[];
-  }
+  })
+  email!: string;
+
+  // Define la columna 'password' con tipo STRING que no puede ser nula y debe ser única.
+  @Column({
+      type: DataType.STRING,
+      allowNull: false,
+      unique: true,
+  })
+  password!: string;
+
+  // Define la relación de uno a muchos con el modelo Task.
+  @HasMany(() => Task)
+  task!: Task[];
+}
