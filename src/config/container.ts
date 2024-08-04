2c3d114f76c30966936d8f404a5c3d5d2c821cad
scorhenao <samcorhenao@gmail.com>
2024-08-04 01:16:22 -0500

    comments explaining container.ts

--- src/config/container.ts
+++ src/config/container.ts
@@ -1,10 +1,21 @@
-import { container } from 'tsyringe';
-import UserService from '../services/userService';
-import UserRepository from '../repositories/userRepository';
-import TaskRepository from '../repositories/taskRepository';
-import TaskService from '../services/taskService';
+import { container } from 'tsyringe';  // Importa el contenedor de inyección de dependencias de la biblioteca tsyringe.
+import UserService from '../services/userService';  // Importa el servicio de usuarios.
+import UserRepository from '../repositories/userRepository';  // Importa el repositorio de usuarios.
+import TaskRepository from '../repositories/taskRepository';  // Importa el repositorio de tareas.
+import TaskService from '../services/taskService';  // Importa el servicio de tareas.
 
-container.registerSingleton<UserRepository>(UserRepository); //Instancia global 
+// Registra UserRepository como un Singleton en el contenedor de inyección de dependencias.
+// Esto asegura que solo haya una instancia global de UserRepository en toda la aplicación.
+container.registerSingleton<UserRepository>(UserRepository);
+
+// Registra UserService como un Singleton en el contenedor de inyección de dependencias.
+// Esto asegura que solo haya una instancia global de UserService en toda la aplicación.
 container.registerSingleton<UserService>(UserService);
+
+// Registra TaskRepository como un Singleton en el contenedor de inyección de dependencias.
+// Esto asegura que solo haya una instancia global de TaskRepository en toda la aplicación.
 container.registerSingleton<TaskRepository>(TaskRepository);
+
+// Registra TaskService como un Singleton en el contenedor de inyección de dependencias.
+// Esto asegura que solo haya una instancia global de TaskService en toda la aplicación.
 container.registerSingleton<TaskService>(TaskService);
