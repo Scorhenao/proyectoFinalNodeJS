b4607e676a743fda0674d538f91f961635c52a4c
scorhenao <samcorhenao@gmail.com>
2024-08-04 01:18:27 -0500

    comments explaining index.ts of models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,5 @@
+// Exporta todo el contenido del archivo 'task.ts' para que pueda ser utilizado en otras partes de la aplicación.
 export * from './task';
+
+// Exporta todo el contenido del archivo 'user.ts' para que pueda ser utilizado en otras partes de la aplicación.
 export * from './user';
