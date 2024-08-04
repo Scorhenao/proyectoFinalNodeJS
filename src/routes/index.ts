1324376fcde18a63fdb46f8c3b02577d59d61d2c
scorhenao <samcorhenao@gmail.com>
2024-08-04 01:22:57 -0500

    comments explaining index.ts of routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,2 @@
-export * from './taskRoutes';
-export * from './userRoutes';
+export * from './taskRoutes'; // Exporta todos los elementos de 'taskRoutes.ts', permitiendo su uso en otros archivos que importen desde este archivo.
+export * from './userRoutes'; // Exporta todos los elementos de 'userRoutes.ts', permitiendo su uso en otros archivos que importen desde este archivo.
